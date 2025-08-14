efd50b893fffb3f07bea49d571798b8dac64e72d
sangharshsawate358-gif <sangharshsawate358@gmail.com>
2025-08-14 21:30:04 +0530

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
 
 // post 
 router.post('/register', async (req,res) => {
@@ -37,72 +39,5 @@ router.post("/login", async (req, res) => {
             age: user.age
     }
 });
-
-
-//  Get user with dynamic age filter
-router.get('/userfilter', async (req, res) => {
-  try {
-    const { age } = req.query;
-    const filter = {};
-    if (age) {
-      filter.age = Number(age);
-    }
-    const users = await User.find(filter);
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching user" });
-  }
-});
-
-
-// only age
-router.get("/users",async (req,res)=>{
-    try{
-        const users = await User.find({age :10});
-    res.json(users);
-        }catch(error){
-        res.status(500).json({error: "Error Fetching user"});
-    }
-
-})
-
-
-// all data
-router.get("/users",async (req,res)=>{
-    try{
-        const users = await User.find();
-    res.json(users);
-        }catch(error){s
-        res.status(500).json({error: "Error!"});
-    }
-
-})
-
-
-//delete user by id 
-router.delete("/users/:id",async(req,res)=>{
-    try{
-        await User.findByIdAndDelete(req.params.id);
-        res.json({message:"User deleted successfully"});
-    }catch(error){
-        res.status(500).json({error:"Error deleting user"})
-    }
-});
-
-
-// update user by id 
-router.put("/users/:id", async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      req.body, 
-      { new: true, runValidators: true } 
-    );
-    res.json({message: "User update successsfully",user:updatedUser});
-  } catch (error) {
-    res.status(500).json({ error: "Error updating user" });
-  }
-});
-
-
+    
 module.exports = router;
