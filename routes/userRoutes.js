a47e39892edff36e2afdf3f80ba19118346f996c
Sangharsh sawate <sangharshsawate358@gmail.com>
2025-08-14 21:47:52 +0530

    Add bcryptjs dependency

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -13,70 +13,36 @@ router.post('/register', async (req,res) => {
     }
 });
 
+router.post("/login", async (req, res) => {
+    try {
+        const { email, password } = req.body;
 
-//  Get user with dynamic age filter
-router.get('/userfilter', async (req, res) => {
-  try {
-    const { age } = req.query;
-    const filter = {};
-    if (age) {
-      filter.age = Number(age);
+        // Find user
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: "User not found" });
+        }
+
+        // Compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Invalid credentials" });
+        }
+
+        res.json({
+            success: true,
+            msg: "Login successful",
+            user: {
+                _id: user._id,
+                name: user.name,
+                email: user.email,
+                age: user.age
+            }
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Server error" });
     }
-    const users = await User.find(filter);
-    res.json(users);
-  } catch (error) {
-    res.status(500).json({ error: "Error fetching user" });
-  }
-});
-
-
-// only age
-router.get("/users",async (req,res)=>{
-    try{
-        const users = await User.find({age :10});
-    res.json(users);
-        }catch(error){
-        res.status(500).json({error: "Error Fetching user"});
-    }
-
-})
-
-
-// all data
-router.get("/users",async (req,res)=>{
-    try{
-        const users = await User.find();
-    res.json(users);
-        }catch(error){s
-        res.status(500).json({error: "Error!"});
-    }
-
-})
-
-
-//delete user by id 
-router.delete("/users/:id",async(req,res)=>{
-    try{
-        await User.findByIdAndDelete(req.params.id);
-        res.json({message:"User deleted successfully"});
-    }catch(error){
-        res.status(500).json({error:"Error deleting user"})
-    }
-});
-
-
-// update user by id 
-router.put("/users/:id", async (req, res) => {
-  try {
-    const updatedUser = await User.findByIdAndUpdate(
-      req.params.id,
-      req.body, 
-      { new: true, runValidators: true } 
-    );
-    res.json({message: "User update successsfully",user:updatedUser});
-  } catch (error) {
-    res.status(500).json({ error: "Error updating user" });
-  }
 });
 
 
